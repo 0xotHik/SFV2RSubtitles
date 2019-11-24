639fa01f28173d79362ebb895257b5a0c69bfc0b
Evgeniy <jenek93@mail.ru>
2019-11-24 15:50:37 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace SFV2RSubtitles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openSourceFileButton = new System.Windows.Forms.Button();
+            this.selectOutFileButton = new System.Windows.Forms.Button();
+            this.inFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.outFilePathTextBox = new System.Windows.Forms.TextBox();
+            this.defaultPostfixTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.changeDefaultPostfixButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.logRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openSourceFileButton
+            // 
+            this.openSourceFileButton.Location = new System.Drawing.Point(683, 22);
+            this.openSourceFileButton.Name = "openSourceFileButton";
+            this.openSourceFileButton.Size = new System.Drawing.Size(75, 23);
+            this.openSourceFileButton.TabIndex = 0;
+            this.openSourceFileButton.Text = "Открыть";
+            this.openSourceFileButton.UseVisualStyleBackColor = true;
+            this.openSourceFileButton.Click += new System.EventHandler(this.openSourceFileButton_Click);
+            // 
+            // selectOutFileButton
+            // 
+            this.selectOutFileButton.Location = new System.Drawing.Point(683, 60);
+            this.selectOutFileButton.Name = "selectOutFileButton";
+            this.selectOutFileButton.Size = new System.Drawing.Size(75, 23);
+            this.selectOutFileButton.TabIndex = 1;
+            this.selectOutFileButton.Text = "Выбрать";
+            this.selectOutFileButton.UseVisualStyleBackColor = true;
+            this.selectOutFileButton.Click += new System.EventHandler(this.selectOutFileButton_Click);
+            // 
+            // inFilePathTextBox
+            // 
+            this.inFilePathTextBox.Location = new System.Drawing.Point(122, 24);
+            this.inFilePathTextBox.Name = "inFilePathTextBox";
+            this.inFilePathTextBox.Size = new System.Drawing.Size(545, 20);
+            this.inFilePathTextBox.TabIndex = 2;
+            // 
+            // outFilePathTextBox
+            // 
+            this.outFilePathTextBox.Location = new System.Drawing.Point(122, 62);
+            this.outFilePathTextBox.Name = "outFilePathTextBox";
+            this.outFilePathTextBox.Size = new System.Drawing.Size(545, 20);
+            this.outFilePathTextBox.TabIndex = 3;
+            // 
+            // defaultPostfixTextBox
+            // 
+            this.defaultPostfixTextBox.Location = new System.Drawing.Point(291, 220);
+            this.defaultPostfixTextBox.Name = "defaultPostfixTextBox";
+            this.defaultPostfixTextBox.Size = new System.Drawing.Size(376, 20);
+            this.defaultPostfixTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Исходный файл";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Итоговый файл";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(245, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Постфикс для итогового файла по умолчанию:";
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Location = new System.Drawing.Point(43, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(700, 2);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // changeDefaultPostfixButton
+            // 
+            this.changeDefaultPostfixButton.Location = new System.Drawing.Point(683, 218);
+            this.changeDefaultPostfixButton.Name = "changeDefaultPostfixButton";
+            this.changeDefaultPostfixButton.Size = new System.Drawing.Size(75, 23);
+            this.changeDefaultPostfixButton.TabIndex = 9;
+            this.changeDefaultPostfixButton.Text = "Изменить";
+            this.changeDefaultPostfixButton.UseVisualStyleBackColor = true;
+            this.changeDefaultPostfixButton.Click += new System.EventHandler(this.changeDefaultPostfixButton_Click);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(32, 103);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(95, 84);
+            this.okButton.TabIndex = 10;
+            this.okButton.Text = "Сделать";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(442, 108);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 13);
+            this.statusLabel.TabIndex = 11;
+            // 
+            // logRichTextBox
+            // 
+            this.logRichTextBox.Location = new System.Drawing.Point(145, 103);
+            this.logRichTextBox.Name = "logRichTextBox";
+            this.logRichTextBox.Size = new System.Drawing.Size(613, 84);
+            this.logRichTextBox.TabIndex = 12;
+            this.logRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 271);
+            this.Controls.Add(this.logRichTextBox);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.changeDefaultPostfixButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.defaultPostfixTextBox);
+            this.Controls.Add(this.outFilePathTextBox);
+            this.Controls.Add(this.inFilePathTextBox);
+            this.Controls.Add(this.selectOutFileButton);
+            this.Controls.Add(this.openSourceFileButton);
+            this.Name = "Form1";
+            this.Text = "SFV2RSubtitles";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openSourceFileButton;
+        private System.Windows.Forms.Button selectOutFileButton;
+        private System.Windows.Forms.TextBox inFilePathTextBox;
+        private System.Windows.Forms.TextBox outFilePathTextBox;
+        private System.Windows.Forms.TextBox defaultPostfixTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button changeDefaultPostfixButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.RichTextBox logRichTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace SFV2RSubtitles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            defaultPostfixTextBox.Text = Properties.Settings.Default.DefaultPostfix;
+        }
+
+        #region GUI
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            logRichTextBox.Text = $"Выполняется.\n" +
+                                  $"Исходный файл: {inFilePathTextBox.Text}\n" +
+                                  $"Итоговый файл: {outFilePathTextBox.Text}\n" +
+                                  $"==========\n";
+
+
+            try
+            {
+                ProcessFile(inFilePathTextBox.Text, outFilePathTextBox.Text);
+            }
+            catch (Exception exception)
+            {
+                logRichTextBox.Text += "ПРОИЗОШЛА ОШИБКА:\n";
+                logRichTextBox.Text += exception;
+                throw;
+            }
+
+            logRichTextBox.Text += "ГОТОВО!";
+        }
+
+        private void openSourceFileButton_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                DialogResult Action = openFileDialog.ShowDialog();
+                if (Action == DialogResult.OK)
+                {
+                    inFilePathTextBox.Text = openFileDialog.FileName;
+
+                    var outFileNameSB = new StringBuilder();
+                    var components = openFileDialog.FileName.Split('.');
+                    components[components.Length - 2] += Properties.Settings.Default.DefaultPostfix;
+                    for (int i = 0; i != components.Length; i++)
+                    {
+                        outFileNameSB.Append(components[i]);
+
+                        if (i + 1 < components.Length)
+                            outFileNameSB.Append(".");
+                    }
+                    outFilePathTextBox.Text = outFileNameSB.ToString();
+                }
+            }
+        }
+
+        private void selectOutFileButton_Click(object sender, EventArgs e)
+        {
+            using var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.ShowDialog();
+            outFilePathTextBox.Text = saveFileDialog.FileName;
+        }
+
+        private void changeDefaultPostfixButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DefaultPostfix = defaultPostfixTextBox.Text;
+            Properties.Settings.Default.Save();
+        }
+        #endregion
+
+        private static void ProcessFile(string inFilePath, string outFilePath)
+        {
+            string[] lines = File.ReadAllLines(inFilePath);
+
+            RemoveRoles(lines);
+            RemoveEmphasis(lines);
+
+            File.WriteAllLines(outFilePath, lines);
+        }
+
+        private static void RemoveRoles(string[] lines)
+        {
+            Regex regex = new Regex("\\[(.*?)\\] ");
+
+            for (int index = 0; index != lines.Length; ++index)
+                lines[index] = regex.Replace(lines[index], "");
+        }
+
+        /// <summary>
+        /// Смотрим, есть ли на второй и далее позициях заглавные буквы.
+        /// Если есть, смотрим следующую букву. Если она не заглавная — эту уменьшаем.
+        /// </summary>
+        private static void RemoveEmphasis(string[] lines)
+        {
+            for (int i = 0; i != lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                    continue;
+
+                // Делим строку по пробелу и процессируем каждое слово
+                //
+                var words = lines[i].Split(' ');
+
+                for (int j = 0; j != words.Length; j++)
+                {
+                    words[j] = RemoveEmphasisInWord(words[j]);
+                }
+
+                // Собираем строку
+                var newLineSB = new StringBuilder(words.Length * 2 - 1);
+                for (int j = 0; j != words.Length; j++)
+                {
+                    newLineSB.Append(words[j]);
+
+                    if (j + 1 < words.Length)
+                        newLineSB.Append(" ");
+                }
+
+                lines[i] = newLineSB.ToString();
+            }
+        }
+
+        private static string RemoveEmphasisInWord(string word)
+        {
+            // Случай одного символа, двух символов (если это сокращение)
+            if (word.Length <= 2)
+                return word;
+
+            if (!char.IsLetter(word[0]) && (!char.IsLetter(word[1])))
+                return word;
+
+            //if (ItIsAcronym(word)) // Если это аббревиатура
+            //    return word;
+
+            for (int j = 1; // Первая буква нам не интересна, это может быть просто название, например
+            j < word.Length - 1; // Последний символ обработаем отдельно
+            j++)
+            {
+                // Если этот символ в верхнем регистре, а следующий за текущим символом символ — в нижнем
+                // То у нас не сокращение, а именно выделенный верхним регистром символ
+
+                var currentChar = word[j];
+
+                // Обрабатываем слово в кавычках
+                if ((j == 1) && (word[0] == '\"'))
+                {
+                    continue;
+                }
+
+
+                var nextChar = word[j + 1];
+                if ((char.IsUpper(currentChar)) && (char.IsLower(nextChar)))
+                {
+                    var chars = word.ToCharArray();
+                    chars[j] = char.ToLower(currentChar);
+                    word = new string(chars);
+                }
+
+
+            }
+
+            // Если последний символ в верхнем регистре, а предыдущий символ — в нижнем, то снова наш случай
+            var lastChar = word[word.Length - 1];
+
+            // Обрабатываем слово в кавычках
+            if (lastChar == '\"')
+                return word;
+
+            var charBeforeLastChar = word[word.Length - 2];
+            if ((char.IsUpper(lastChar)) && (char.IsLower(charBeforeLastChar)))
+            {
+                var chars = word.ToCharArray();
+                chars[word.Length - 1] = char.ToLower(lastChar);
+                word = chars.ToString();
+            }
+
+            return word;
+        }
+
+        private static bool ItIsAcronym(string word)
+        {
+            var chars = word.ToCharArray();
+
+            foreach (var symbol in chars)
+            {
+                if (symbol == '\"')
+                    continue;
+
+                if (char.IsLower(symbol))
+                    return false;
+            }
+
+            return true;
+        }
+
+    }
+}
